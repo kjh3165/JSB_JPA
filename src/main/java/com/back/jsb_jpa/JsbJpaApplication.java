b3a6e2fe1060ff9d48fd4fad20437a89840263f6
kjh3165 <jhwk109@gmail.com>
2026-05-04 09:48:07 +0900

    세팅

---
+++ src/main/java/com/back/jsb_jpa/JsbJpaApplication.java
@@ -0,0 +1,13 @@
+package com.back.jsb_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsbJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsbJpaApplication.class, args);
+    }
+
+}
