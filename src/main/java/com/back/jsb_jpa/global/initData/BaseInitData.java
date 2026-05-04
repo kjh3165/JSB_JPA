5b187b915f1e0fc7da0a135c21467b13bb09f176
kjh3165 <jhwk109@gmail.com>
2026-05-04 11:39:00 +0900

    Post 객체를 생성하여 리포지터리를 통해서 save(내부적으로는 insert 수행됨)

--- src/main/java/com/back/jsb_jpa/global/initData/BaseInitData.java
+++ src/main/java/com/back/jsb_jpa/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.jsb_jpa.global.initData;
 
+import com.back.jsb_jpa.domain.post.post.entity.Post;
 import com.back.jsb_jpa.domain.post.post.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -14,7 +15,9 @@ public class BaseInitData {
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            postRepository.count();
+            Post post = new Post();
+            post.setTitle("제목 1");
+            postRepository.save(post);
         };
     }
 }
