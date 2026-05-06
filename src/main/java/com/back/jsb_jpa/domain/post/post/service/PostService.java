343fdbf0b1633fc5bc5f77a82bc43d0dd3875efb
kjh3165 <jhwk109@gmail.com>
2026-05-06 10:21:39 +0900

    PostService에도 @RequiredArgsConstructor 적용

--- src/main/java/com/back/jsb_jpa/domain/post/post/service/PostService.java
+++ src/main/java/com/back/jsb_jpa/domain/post/post/service/PostService.java
@@ -2,16 +2,16 @@ package com.back.jsb_jpa.domain.post.post.service;
 
 import com.back.jsb_jpa.domain.post.post.entity.Post;
 import com.back.jsb_jpa.domain.post.post.repository.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class PostService {
 
-    @Autowired
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
 
     public Post save(Post post) {
         return postRepository.save(post);
