64cf57ae94a6e6d269bbec515f3bcaece1f8b997
kjh3165 <jhwk109@gmail.com>
2026-05-06 12:28:30 +0900

    모든 엔티티의 기본 속성을 BaseEntity 클래스에 모아두고 상속받아서 사용, @MappedSuperClass

--- src/main/java/com/back/jsb_jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/back/jsb_jpa/domain/post/post/entity/Post.java
@@ -1,31 +1,20 @@
 package com.back.jsb_jpa.domain.post.post.entity;
 
-import jakarta.persistence.*;
+import com.back.jsb_jpa.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity // 아래 구조대로 DB 테이블(컬럼)을 만들어야 한다.
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
-    @Id // PK
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
-    private int id; // INT
+public class Post extends BaseEntity {
     private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
     private String content; // TEXT
-    @CreatedDate
-    private LocalDateTime createDate; // 최초 INSERT 시 자동으로 현재 시간으로 설정됨
-    @LastModifiedDate
-    private LocalDateTime modifyDate; // INSERT/UPDATE 시 자동으로 현재 시간으로 설정됨
 
     public Post(String title, String content) {
         this.title = title;
