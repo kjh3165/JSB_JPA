021e351f918ec2d0d2a21b363ed0dd9281f0defa
kjh3165 <jhwk109@gmail.com>
2026-05-06 11:44:22 +0900

    @CreatedDate, @LastModifiedDate 도입하여 등록날짜와 수정날짜가 스마트하게 갱신되도록

--- src/main/java/com/back/jsb_jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/back/jsb_jpa/domain/post/post/entity/Post.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity // 아래 구조대로 DB 테이블(컬럼)을 만들어야 한다.
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id // PK
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
@@ -18,13 +22,13 @@ public class Post {
     private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
     private String content; // TEXT
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+    @CreatedDate
+    private LocalDateTime createDate; // 최초 INSERT 시 자동으로 현재 시간으로 설정됨
+    @LastModifiedDate
+    private LocalDateTime modifyDate; // INSERT/UPDATE 시 자동으로 현재 시간으로 설정됨
 
     public Post(String title, String content) {
         this.title = title;
         this.content = content;
-        this.createDate = LocalDateTime.now();
-        this.modifyDate = this.createDate;
     }
 }
