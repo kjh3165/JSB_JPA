493cb3dbb7ec8dd4d6abbb1627d8b3e08a922136
kjh3165 <jhwk109@gmail.com>
2026-05-07 10:43:05 +0900

    test 프로파일 상태에서만 작동하는 testInitDataApplicationRunner 빈을 통해서 테스트 전용 샘플 데이터를 추가로 생성

---
+++ src/main/java/com/back/jsb_jpa/global/initData/TestInitData.java
@@ -0,0 +1,33 @@
+package com.back.jsb_jpa.global.initData;
+
+import com.back.jsb_jpa.domain.post.post.entity.Post;
+import com.back.jsb_jpa.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+@RequiredArgsConstructor
+public class TestInitData {
+    @Autowired
+    @Lazy
+    private TestInitData self;
+    private final PostService postService;
+
+    @Bean
+    ApplicationRunner testInitDataApplicationRunner() {
+        return args -> {
+            if (postService.count() >= 4) return;
+
+            Post post1 = postService.write("제목 3", "내용 3");
+            Post post2 = postService.write("제목 4", "내용 4");
+
+            System.out.println("테스트 데이터가 초기화 되었습니다.");
+        };
+    }
+}
